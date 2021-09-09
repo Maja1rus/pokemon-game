e5847f17d559bfa524e0607587924cef5dcb4c50
Maja1rus <nalimoff.deniss@gmail.com>
2021-09-09 20:56:33 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,32 +3,19 @@ import Header from './components/Header/Header'
 import Layout from './components/Layout/Layout'
 import Footer from './components/Footer/Footer'
 
-// import bg from './assets/bg3.jpg'
-
-
-// const styleBg = {
-//   background: bg
-// }
-
-// console.log(styleBg);
+import bg from './assets/bg3.jpg'
 
 const App = () => {
     return (
         <div>
-            <Header
-            />
+            <Header />
+            <Layout title="First Layout" desc="First description" imgBg={bg} />
             <Layout
-                title="Первый Layout"
-                colorBg
+                title="Second Layout"
+                desc="Second description"
+                colorBg="#546747"
             />
-            <Layout
-                title="Второй Layout"
-                
-            />
-            <Layout
-                title="Третий Layout"
-                colorBg
-            />
+            <Layout title="Third Layout" desc="Third description" imgBg={bg} />
             <Footer />
         </div>
     )
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import style from './Layout.module.css'
 
-import bg from '../../assets/bg3.jpg'
-
-const Layout = ({ title, desc, colorBg }) => {
-    // const styleRoot = colorBg ? {background: '#357e51'} : {}
-    const styleRoot = colorBg ? {background: `url(${bg})`} : {background: '#357e51'}
+const Layout = ({ title, desc, imgBg, colorBg}) => {
+    const styleRoot = imgBg ? {background: `url(${imgBg})`} : {background: `${colorBg}`}
     return (
         <div>
-            <section className={style.root} style={(styleRoot)}>
+            <section className={style.root} style={styleRoot}>
                 <div className={style.wrapper}>
                     <article>
                         <div className={style.title}>
                             <h3>{title}</h3>
                             <span className={style.separator}></span>
                         </div>
-                        <div className={(style.desc, style.full)}>
+                        <div className={`${style.desc} ${style.full}`}>
                             <p>{desc}</p>
                         </div>
                     </article>
